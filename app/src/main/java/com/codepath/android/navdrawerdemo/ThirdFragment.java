1265dec1c7d19b185427b6035f8832a7b19122ac
nidhi1608 <nidhi.1608@gmail.com>
2015-02-19 20:44:46 -0800

    initial commit

---
+++ app/src/main/java/com/codepath/android/navdrawerdemo/ThirdFragment.java
@@ -0,0 +1,20 @@
+package com.codepath.android.navdrawerdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ThirdFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_item, container, false);
+        TextView tvNo = (TextView) rootView.findViewById(R.id.tvNo);
+        tvNo.setText("Third Fragment");
+        return rootView;
+    }
+}
